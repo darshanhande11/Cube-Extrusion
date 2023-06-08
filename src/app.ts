46bd3c5b954a68aaddbdb35763a05ab27fdf8ea3
Darshan Hande <darshanhande11@gmail.com>
2023-06-08 10:59:56 +0530

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -154,18 +154,6 @@ class App {
             }
         }
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
-                }
-            }
-        });
-
         // run the main render loop
         engine.runRenderLoop(() => {
             scene.render();
